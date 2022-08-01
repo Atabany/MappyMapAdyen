43f0d9662b20bc9964ea35c1a06bff80888bc6e3
Atabany <mohamed.elatabany@leanscale.com>
2022-08-02 00:19:36 +0200

    initial

---
+++ MappyMapAdyenTask/MappyMapAdyenTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MappyMapAdyenTask
+//
+//  Created by Mohamed Elatabany on 02/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
